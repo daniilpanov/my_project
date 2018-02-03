d47b3171d967d74a843cad45846e11de8a15fb51
daniil_panov <daniil_panov2005@mail.ru>
2018-02-03 19:47:29 +0300

    Новый проект

--- menu.php
+++ menu.php
@@ -1,1 +1,12 @@
-<a class="content" href="">dgfhk</a>
+<div class="first">
+    <img src="img/Без%20названия.png">
+</div>
+<div class="first">
+    <h1>MY PROJECT</h1>
+</div>
+<div class="container">
+    <a class="content" href="?index">Главная</a>
+    <a class="content" href="?reviews">Отзывы</a>
+    <a class="content" href="?info">О сайте</a>
+    <a class="content" href="?admin">Войти как администратор</a>
+</div>
