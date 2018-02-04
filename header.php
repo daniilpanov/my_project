24d31a9b414fed7c36cac98c5cbbf88ee9315e35
daniil_panov <daniil_panov2005@mail.ru>
2018-02-04 16:00:36 +0300

    Новый проект

--- header.php
+++ header.php
@@ -12,7 +12,8 @@ function __autoload($name)
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Главная страница</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="styles/style.css">
 </head>
-</html>
--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
-<div class="first">
-    <img src="img/Без%20названия.png">
-</div>
-<div class="first">
+<div class="col-md-4 first"><img src="img/Без%20названия.png"></div>
+<div class="col-md-7 first">
     <h1>MY PROJECT</h1>
 </div>
-<div class="container">
+<hr size="2px">
+
+<div class="jumbotron">
     <a class="content" href="?index">Главная</a>
     <a class="content" href="?reviews">Отзывы</a>
     <a class="content" href="?info">О сайте</a>
