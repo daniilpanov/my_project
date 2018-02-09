d8a2c575c5c691e1ae2dd96590f3773dd450967f
daniil <daniil_panov@mail.ru>
2018-02-09 17:13:38 +0300

    обн

---
+++ admin/app/menu.php
@@ -0,0 +1,10 @@
+<hr size="3px">
+<div class="jumbotron">
+    <b>
+        <i>
+            <a class="content" href="index.php">Главная</a>
+            <a class="content" href="?page=reviews">Отзывы</a>
+            <a class="content" href="?page=info">О сайте</a>
+        </i>
+    </b>
+</div>
