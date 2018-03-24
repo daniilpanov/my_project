ac1894f7f20b3429b7b9838c092e3ca299226c36
daniil_panov <daniil_panov2005@mail.ru>
2018-03-24 21:41:21 +0300

    Добавили шифр

--- admin/Views/VLogin.php
+++ admin/Views/VLogin.php
@@ -1,18 +1,21 @@
 <!--Просто форма-->
-
-<form method="post">
-    <h2>Для того, чтобы войти как админисратор,<br>Вам надо заполнить следующие поля:</h2>
-    <table id="login">
-        <tbody>
-        <tr>
-            <td><u>Фамилия, имя</u><input type="text" placeholder="Ваши фамилия, имя" name="name"></td>
-            <td><u>E-mail</u><input type="email" placeholder="Ваш e-mail" name="email"></td>
-        </tr>
-        <tr>
-            <td><u>Логин</u><input type="text" name="login" placeholder="Логин">
-            <td><u>Пароль</u><input type="password" name="password" placeholder="Пароль"></td>
-        </tr>
-        </tbody>
-    </table>
-    <input type="submit" name="authorisation" value="Продолжить">
-</form>
+<div id="page">
+    <center>
+        <form method="post" class="login">
+            <h2>Для того, чтобы войти как админисратор,<br>Вам надо заполнить следующие поля:</h2>
+            <table id="login">
+                <tbody>
+                <tr>
+                    <td><u>ФИ</u></td><td><input type="text" placeholder="Ваши фамилия, имя" name="name"></td>
+                    <td><u>E-mail</u></td><td><input type="email" placeholder="Ваш e-mail" name="email"></td>
+                </tr>
+                <tr>
+                    <td><u>Логин</u></td><td><input type="text" name="login" placeholder="Логин"></td>
+                    <td><u>Пароль</u></td><td><input type="password" name="password" placeholder="Пароль"></td>
+                </tr>
+                </tbody>
+            </table>
+            <input type="submit" name="authorisation" value="Продолжить">
+        </form>
+    </center>
+</div>
