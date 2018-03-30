0d34d06f5d8de64864830ea4464d7a9069b5d72f
daniil_panov <daniil_panov2005@mail.ru>
2018-03-30 17:22:13 +0300

    Скачал Bootstrap

--- body.php
+++ body.php
@@ -5,7 +5,7 @@
             <tr>
                 <!--Логотип сайта-->
                 <td id="logo" class="col-3">
-                    <img src="img/compass_pic.png" width="100" height="100">
+                    <img src="img/compass_pic.png" width="100" height="100" border="0">
                 </td>
                 <!--Название сайта-->
                 <td id="siteName" class="col-8">
@@ -20,6 +20,7 @@ require_once "views/VMenu.php";
 echo "<div class='content'>";
 echo $content['content'];
 echo "</div>";
+
 // Router
 if ($_GET)
 {
