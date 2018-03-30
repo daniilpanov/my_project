c6150893e756fed97f234366b39cd2105628688f
daniil_panov <daniil_panov2005@mail.ru>
2018-03-30 17:43:01 +0300

    Скачал Bootstrap

--- admin/views/VMenu.php
+++ admin/views/VMenu.php
@@ -1,13 +1,13 @@
 <div class="checkYourDoing">
     <hr class="forMenu" size="18px">
-    <input type="submit" name="delete" id="delete" hidden><label for="delete" class="doing" title="Удалить выбранное"><i class="icon-trash icon-large"></i></label>
+    <a class="doing" href="index.php" title="Вернуться на главную страницу"><i class="icon-home icon-large"></i></a>
     <a class="doing" href="?doing=seeMenu" title="Просмотреть все меню"><i class="icon-tasks icon-large"></i></a>
     <a class="doing" href="?doing=seePages" title="Просмотреть все страницы"><i class="icon-th-list icon-large"></i></a>
-    <a class="doing" href="index.php" title="Вернуться на главную страницу"><i class="icon-home icon-large"></i></a>
-    <a class="doing" href="?doing=create" title="Создать"><i class="icon-edit icon-large"></i></a>
-    <a class="doing" href="?doing=seeFav" title="Быстрый доступ"><i class="icon-book icon-large"></i></a>
-    <a class="doing" href="?doing=addFav" title="Редактировать быстрый доступ"><i class="icon-list-alt icon-large"></i></a>
+    <a class="doing" href="?doing=createMenu" title="Создать меню"><i class="icon-edit icon-large"></i></a>
+    <a class="doing" href="?doing=createPage" title="Создать страницу"><i class="icon-book icon-large"></i></a>
+    <input type="submit" name="delete" id="delete" hidden><label for="delete" class="doing" title="Удалить выбранное"><i class="icon-trash icon-large"></i></label>
 
+    <a class="doing" href='?page=base' title="Выйти"><i class="icon-off icon-large"></i></a>
     <a class="doing" href='?page=logout' title="Выйти"><i class="icon-off icon-large"></i></a>
 </div>
 <div class="pages menu">
