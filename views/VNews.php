b92b753db28db87481ad64ed98696d0c18784f3f
daniil_panov <daniil_panov2005@mail.ru>
2018-04-20 22:26:45 +0300

    Сделал новости

--- views/VNews.php
+++ views/VNews.php
@@ -1,20 +1,30 @@
 <?php
 $news = new app\classes\CNews();
 ?>
+<table class="news">
+    <tr>
+        <td class="center"><strong>Главные новости:</strong></td>
+    </tr>
+    <tr>
+        <td>
+            <ul>
+                <?php
+                $allNews = $news->getNews();
 
-<ul id="news">
-    <?php
-    $allNews = $news->getNews();
+                foreach ($allNews as $key => $value)
+                {
+                    echo "<li><a href='?news={$value['id']}'>{$value['name']}</a></li>";
+                    echo "<ul>";
+                    echo "<li>{$value['description']}</li>";
+                    echo "</ul>";
+                }
 
-    foreach ($allNews as $key => $value)
-    {
-        echo "<li><a href='?news={$value['id']}'>{$value['name']}</a></li>";
-        echo "<ul>";
-        echo "<li>{$value['description']}</li>";
-        echo "</ul>";
-    }
-
-    $countNews = $news->counter();
-    ?>
-</ul>
-<?=$countNews?>
+                $countNews = $news->counter();
+                ?>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td><u><b>Общее количество новостей: <?=$countNews?>!</b></u></td>
+    </tr>
+</table>
