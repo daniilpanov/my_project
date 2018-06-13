8610899ac78f2a073f4a83103cf759ec6dc717fb
daniil_panov <daniil_panov2005@mail.ru>
2018-06-13 22:09:24 +0300

    Начал делать отзывы + скопировал с Вашего проекта VHelp.php(надо сделать нормально); надо сделать "пагинацию"

--- admin/views/VLogin.php
+++ admin/views/VLogin.php
@@ -9,7 +9,7 @@
                 <label>Ваш логин: <input type="text" name="login" placeholder="Ваш логин: "></label>
                 <label>Ваш пароль: <input type="password" name="password" placeholder="Ваш пароль: "></label>
             </blockquote>
-            <input type="submit" value="Продолжить">
+            <input type="submit" name="authorisation" value="Продолжить">
         </div>
     </form>
 </div>
